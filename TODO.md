33d942f544bc31f28f61f9edc79dd37f915b25b9
Bati <no-reply@batijs.dev>
2025-12-24 13:42:42 +0100

    scaffold Vike app with Bati

---
+++ TODO.md
@@ -0,0 +1,6 @@
+Follow the steps below to finish setting up your application.
+
+## plausible.io
+
+Update `pages/+Head.tsx` to properly set `data-domain` or replace the script with the one from your Plausible account.
+
---
+++ pages/+Head.tsx
@@ -0,0 +1,15 @@
+// https://vike.dev/Head
+
+import logoUrl from "../assets/logo.svg";
+
+export function Head() {
+  return (
+    <>
+      <link rel="icon" href={logoUrl} />
+
+      {/* See https://plausible.io/docs/plausible-script */}
+      {/* TODO: update data-domain */}
+      <script defer data-domain="yourdomain.com" src="https://plausible.io/js/script.js"></script>
+    </>
+  );
+}
